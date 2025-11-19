457deb299fca9c9c404e77979b3e2f28a277dfb1
Vitor <vitordn.va@gmail.com>
2025-11-19 10:21:10 -0300

    refactor: replace SVG icons with Ionicons components

--- components/icons/HomeIcon.tsx
+++ components/icons/HomeIcon.tsx
@@ -1,26 +1,12 @@
-import * as React from "react";
-import Svg, { Path, SvgProps } from "react-native-svg";
+import { Ionicons } from "@expo/vector-icons";
 
 export function HomeIcon({
-  width = 32,
-  height = 32,
+  size = 32,
   color = "#000000",
   ...props
-}: SvgProps) {
-  return (
-    <Svg
-      width={width}
-      height={height}
-      viewBox="0 0 16 16"
-      fill="none"
-      {...props}
-    >
-      <Path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M8 0L0 6V8H1V15H4V10H7V15H15V8H16V6L14 4.5V1H11V2.25L8 0ZM9 10H12V13H9V10Z"
-        fill={color}
-      />
-    </Svg>
-  );
+}: {
+  size?: number;
+  color?: string;
+}) {
+  return <Ionicons name="home" size={size} color={color} {...props} />;
 }
--- components/icons/IPhoneIcon.tsx
+++ components/icons/IPhoneIcon.tsx
@@ -1,18 +1,12 @@
-import * as React from "react";
-import Svg, { Path, SvgProps } from "react-native-svg";
+import { Ionicons } from "@expo/vector-icons";
 
 export function IPhoneIcon({
-  width = 32,
-  height = 32,
+  size = 32,
   color = "#000000",
   ...props
-}: SvgProps) {
-  return (
-    <Svg width={width} height={height} viewBox="-9.5 0 32 32" {...props}>
-      <Path
-        d="M2.375 4h8.281c1.313 0 2.375 1.063 2.375 2.375v19.25c0 1.313-1.063 2.375-2.375 2.375h-8.281c-1.313 0-2.375-1.063-2.375-2.375v-19.25c0-1.313 1.063-2.375 2.375-2.375zM1.531 23.281h9.969v-14.781h-9.969v14.781zM6.5 26.563c0.75 0 1.313-0.563 1.313-1.281 0-0.75-0.563-1.313-1.313-1.313-0.719 0-1.281 0.563-1.281 1.313 0 0.719 0.563 1.281 1.281 1.281z"
-        fill={color}
-      />
-    </Svg>
-  );
+}: {
+  size?: number;
+  color?: string;
+}) {
+  return <Ionicons name="phone-portrait" size={size} color={color} {...props} />;
 }
